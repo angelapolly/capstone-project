c973a57d98eb1ea0238f25aa489760a6258766b8
Angela Polly <agpolly@gmail.com>
2022-03-10 18:28:16 -0500

    User creates mood - display list of moods

---
+++ src/main/java/org/launchcode/capstoneproject/controllers/HomeController.java
@@ -0,0 +1,15 @@
+package org.launchcode.capstoneproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+public class HomeController {
+
+    private static List<String> moods = new ArrayList<>();
+
+}
---
+++ src/main/java/org/launchcode/capstoneproject/controllers/MoodController.java
@@ -0,0 +1,35 @@
+package org.launchcode.capstoneproject.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Controller
+@RequestMapping("moods")
+public class MoodController {
+
+    private static List<String> moods = new ArrayList<>();
+
+    //Lives at /moods
+    @GetMapping
+    public String displayAllMoods(Model model) {
+        model.addAttribute("moods", moods);
+        return "mood-list.html";
+    }
+
+    //Lives at moods/create
+    @GetMapping("create")
+    public String renderCreateMoodForm() {
+        return "mood-create.html";
+    }
+
+    //Lives at moods/create
+    @PostMapping("create")
+    public String createMood(@RequestParam String moodName) {
+        moods.add(moodName);
+        return "redirect:";
+    }
+}
