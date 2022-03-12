90e42735e4de43c593d697290e16452c4262a812
Angela Polly <agpolly@gmail.com>
2022-03-12 01:00:23 -0500

    Create unique Id

--- src/main/java/org/launchcode/capstoneproject/models/Mood.java
+++ src/main/java/org/launchcode/capstoneproject/models/Mood.java
@@ -1,8 +1,12 @@
 package org.launchcode.capstoneproject.models;
 
 import java.awt.*;
+import java.util.Objects;
 
 public class Mood {
+
+    private int id;
+    private static int nextId = 1;
 
     private String name;
     private String moodColor;
@@ -10,6 +14,8 @@ public class Mood {
     public Mood(String name, String moodColor) {
         this.name = name;
         this.moodColor = moodColor;
+        this.id = nextId;
+        nextId++;
     }
 
     public String getName() {
@@ -26,6 +32,23 @@ public class Mood {
 
     public void setMoodColor(String moodColor) {
         this.moodColor = moodColor;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Mood mood = (Mood) o;
+        return id == mood.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 
     @Override
